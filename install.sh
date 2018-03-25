d3702c3f3d74bd06d765370486d28d0ece51f85f
Mark W. Christiansen <28930413+mwchristiansen@users.noreply.github.com>
2018-03-25 16:46:20 -0700

    copy .vimrc and rename installation

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
+vim +PluginInstall +qall
+
+cp .vimrc ~/.vimrc
