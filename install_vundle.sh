0d26fe295e28502694ca6b878a5615e39c8cdaee
mark <28930413+mwchristiansen@users.noreply.github.com>
2018-03-25 16:49:49 -0700

    fix nerdtree installation and delete old install

--- install_vundle.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
-vim +PluginInstall +qall
